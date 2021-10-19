709c32c667617608fe1c70817f567085d95edfc5
slava1605 <91557645+slava1605@users.noreply.github.com>
2021-10-19 19:05:23 +0300

    my coop filter 2

--- src/helpers/testRacing.js
+++ src/helpers/testRacing.js
@@ -615,5 +615,22 @@ export const filterOptions = {
 	racingDistances: [100, 120, 140, 160, 180, 200],
 	terrains: ['Dirt', 'Grass', 'Road', 'Rock', 'Sand', 'Snow', 'Track'],
 	stocks: ['Spicy', 'Robust', 'Fresh', 'Crisp', 'Tender', 'Bland'],
+	situations: ['Barn', 'Race Pen', 'Racing'],
+	genders: ['Hen', 'Rooster'],
+	colors: ['Bald Chicken', 'Black', 'Cherry Dusk', 'Classic', 'Eggshell', 'English Mustard',
+		'Istanblue', "Joker's Jade", "Manic Mint", "Merah Red", "Orange Will", "Purple  Wine",
+		"Robot", "Rose", "Royal Violet",  "Sapphire", "Screamin Green", "Shocking Pink", "Wild Moss",
+	],
+	combs: ["Black", "Blue", "Candy", "Green", "Orange", "Pink", "Purple", "Red", "Studs", "Teal",
+		"White", "Yellow"
+	],
+	wattles: ["Black", "Blue", "Candy", "Green", "Orange", "Pink", "Purple", "Red", "Teal", "White", "Yellow"],
+	beaks: ["Black", "Gold", "White", "Orange", "Yellow"],
+	accessories: ["None", "Ring", "Vampire"],
+	eyes: ["Alien", "Angry", "Beauty", "Bloodshot", "Bulging", "Cockeyed", "Crosseyed", "Determined",
+		"Exhausted", "Eyepatch", "Lizard", "Robot", "Sad", "Shocked", "Sleepy"
+	],
+	backgrounds: ["Amethyst", "Autumn", "Flesh", "Lava", "Lilac", "Ocean", "Spring", "Stone", "Summer", "Winter"], 
+	stripes: ["Yes", "No"]
 };
 /************************************************/
